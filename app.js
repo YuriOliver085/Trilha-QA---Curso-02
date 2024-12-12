72a2d30e86e7e1ba33183097739c71478b93ce3b
YuriOliver085 <armandoyuri2013@yahoo.com.br>
2024-12-12 17:42:38 -0300

    Funcoes

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-//Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Hora do Desafio';
+// Criar uma função que exibe "Olá, mundo!" no console.
+function exibir() {
+    console.log("Olá, mundo!");
+  }
+  exibir();
 
-// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado:
-function exibirMensagem() {
-    console.log('O botão foi clicado!')
-}
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console. 
+function exibirNome(nome) {
+    console.log(`Olá, ${nome}!`);
+  }
+  exibirNome("Yuri");
 
-// Crie uma função que exiba no console a mensagem Eu amo Js sempre que o botão Console for pressionado.
-function exibirAlerta() {
-    alert('Eu amo Js')
-}
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function calcularDobro(numero) {
+    return numero * 2;
+  }
+  
+  let resultado = calcularDobro(7);
+  console.log(resultado);
 
-// Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você:
-function exibirPrompt() {
-    let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil:')
-    alert(`Estive em ${nomeDaCidade} e lembrei de você`)
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function calcularMediaMedia(a, b, c) {
+    return (a + b + c) / 3;
+  }
+  
+  let media = calcularMedia(5, 7, 13);
+  console.log(media);
 
-// Ao clicar no botão soma, peça 2 números e exiba o resultado da soma em um alerta.
-function somandoDoisNumeros() {
-    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
-    let segundoNumero = parseInt(prompt('Digite o segundo número'));
-    let resultado = primeiroNumero + segundoNumero;
-    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function maior(a, b) {
+    return a > b ? a : b;
+  }
+  
+  let maiorNumero = maior(8, 10);
+  console.log(maiorNumero);
+
+// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo.
+function multiplicarPorSi(numero) {
+    return numero * numero;
+  }
+  
+  let resultado1 = multiplicarPorSi(2);
+  console.log(resultado1); 
